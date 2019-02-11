6bb6e02e68cfbed0ef4aa377e96db206f12a344e
rbm717 <47369758+rbm717@users.noreply.github.com>
2019-02-11 14:17:31 -0800

    Update A3_1_3_Part2.c

--- A3_1_3_Part2.c
+++ A3_1_3_Part2.c
@@ -1,22 +1,36 @@
 
+#pragma config(Sensor, in1,    lineFollower,   sensorLineFollower)
+#pragma config(Sensor, in2,    potentiometer,  sensorPotentiometer)
+#pragma config(Sensor, in3,    lightSensor,    sensorReflection)
+#pragma config(Sensor, dgtl1,  limitSwitch,    sensorTouch)
+#pragma config(Sensor, dgtl2,  bumpSwitch,     sensorTouch)
+#pragma config(Sensor, dgtl3,  quad,           sensorQuadEncoder)
+#pragma config(Sensor, dgtl5,  sonar,          sensorSONAR_inch)
+#pragma config(Sensor, dgtl12, red,            sensorLEDtoVCC)
+#pragma config(Motor,  port1,           flashlight,    tmotorVexFlashlight, openLoop, reversed)
+#pragma config(Motor,  port2,           rightMotor,    tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port3,           leftMotor,     tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port9,           servoMotor,    tmotorServoStandard, openLoop)
 /*
-  Project Title:
-  Team Members:
-  Date:
-  Section:
+  Project Title: Output Output Output
+  Team Members: Raymond Metzger, Ryan Potter, Robbie Vannucchi, Nick Franzwa
+  Date: 2/1/19
+  Section: A3_1_3 Part 2
 
 
-  Task Description:
+  Task Description: Turn on motors once bump switch pressed
 
 
   Pseudocode:
-
 */
 
 task main()
 {                                     //Program begins, insert code within curly braces
 
-
-
+  untilBump(bumpSwitch);
+	startMotor(rightMotor, 63);
+	startMotor(leftMotor, 63);
+	wait(5);
+	stopMotor(rightMotor);
 
 }
